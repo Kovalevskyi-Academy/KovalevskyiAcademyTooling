70720c6afd97fffd365bde15bf9e8dab6efb98de
straybro <german.malinovsky@gmail.com>
2021-01-04 18:06:41 +0100

    feat: add dummy assignment

---
+++ assignments/bootcamp/java/level0/w0d0/src/main/java/com/kovalevskyi/academy/codingbootcamp/level0/w0d0/Main.java
@@ -0,0 +1,7 @@
+package com.kovalevskyi.academy.codingbootcamp.level0.w0d0;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
---
+++ assignments/bootcamp/java/level0/w0d0/src/test/java/com/kovalevskyi/academy/codingbootcamp/level0/w0d0/MainTest.java
@@ -0,0 +1,12 @@
+package com.kovalevskyi.academy.codingbootcamp.level0.w0d0;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+  @Test
+  public void testGreen() {
+    assertTrue(false);
+  }
+}
